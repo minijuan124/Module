09542ea0fbbca61e61fe6e081c67ab132436031a
Jesús Rivera <jesusariverad09@gmail.com>
2025-11-20 13:00:06 -0400

    \Deploy: clean publish 2025-11-20T13:00:06.7536113-04:00" -q

--- scripts/copyIndex.js
+++
@@ -1,3 +0,0 @@
-// copyIndex.js: removed as part of gh-pages cleanup
-console.log('copyIndex.js removed: publishing helpers disabled.');
-process.exit(0);
--- scripts/setBaseHref.js
+++
@@ -1,3 +0,0 @@
-// setBaseHref.js: removed as part of gh-pages cleanup
-console.log('setBaseHref.js removed: publishing helpers disabled.');
-process.exit(0);
