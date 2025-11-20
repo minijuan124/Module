660e60b75683bc120481f8e8bf6f62ce3feb3daa
Jesús Rivera <jesusariverad09@gmail.com>
2025-11-20 12:55:49 -0400

    Deploy: clean publish 2025-11-20T12:55:49.1854574-04:00

---
+++ scripts/fixAssetsRuntime.js
@@ -0,0 +1,3 @@
+// fixAssetsRuntime.js: removed as part of gh-pages cleanup
+console.log('fixAssetsRuntime.js removed: publishing helpers disabled.');
+process.exit(0);
--- scripts/setBaseHref.js
+++ scripts/setBaseHref.js
@@ -1,19 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-const distDir = path.resolve(__dirname, '..', 'dist');
-const files = ['index.html', 'index.csr.html', 'index.ssr.html'];
-const newBase = '/Module/';
-
-files.forEach((file) => {
-  const filePath = path.join(distDir, file);
-  if (!fs.existsSync(filePath)) return;
-  let content = fs.readFileSync(filePath, 'utf8');
-  // Replace any existing <base ...> tag with the correct base
-  content = content.replace(/<base[^>]*href=["'][^"']*["'][^>]*>/i, `<base href="${newBase}">`);
-  fs.writeFileSync(filePath, content, 'utf8');
-  console.log(`Updated base href in ${file}`);
-});
-
-// Exit 0 even if nothing changed
+// setBaseHref.js: removed as part of gh-pages cleanup
+console.log('setBaseHref.js removed: publishing helpers disabled.');
 process.exit(0);
